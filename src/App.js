23ba12a06bf26e19fdf6f578b1c10597dbed6eb0
theshanind <indurangatheshan8@gmail.com>
2026-01-19 13:58:45 +0530

    register page implementation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import LandingPage from './component/landing_page/landing_page';
 import LoginPage from './component/login_page/login_page';
+import RegisterPage from './component/register_page/register_page';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
         </Routes>
       </div> 
     </Router>
