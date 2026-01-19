e4a17ce8d93d0018c93376ff6f02a2d14ac53538
theshanind <indurangatheshan8@gmail.com>
2026-01-19 21:34:41 +0530

    dashboard page implementation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import LandingPage from './component/landing_page/landing_page';
 import LoginPage from './component/login_page/login_page';
 import RegisterPage from './component/register_page/register_page';
+import DashboardPage from './component/dashboard_page/dashboard_page';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<LandingPage/>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/register' element={<RegisterPage/>}/>
+          <Route path='/dashboard' element={<DashboardPage/>}/>
         </Routes>
       </div> 
     </Router>
