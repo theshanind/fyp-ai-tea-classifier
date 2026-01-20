8949e83e33edbefcac19fc82cf42907dc943dcb9
theshanind <indurangatheshan8@gmail.com>
2026-01-20 15:16:15 +0530

    react router dom in landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router,Route,Routes }from 'react-router-dom';
 import LandingPage from './component/landing_page/landing_page';
+
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div> 
+    <Router>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+      </Routes>
+    </Router>
+    
   );
 }
 
